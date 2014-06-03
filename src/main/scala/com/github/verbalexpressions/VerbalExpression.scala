4b2e1c5e7fc17c046c3e98780c1f2256a76f0833
pathikrit <pathikritbhowmick@msn.com>
2014-06-03 15:09:41 -0700

    group similar methods

--- src/main/scala/com/github/verbalexpressions/VerbalExpression.scala
+++ src/main/scala/com/github/verbalexpressions/VerbalExpression.scala
@@ -9,39 +9,33 @@ case class VerbalExpression(prefix: String = "", expression: String = "", suffix
   def add(value: String) = copy(expression = expression + value)
 
   def andThen(value: String) = add(s"(${quote(value)})")
-
   def then = andThen _
-
   def find = andThen _
 
   def maybe(value: String) = add(s"(${quote(value)})?")
 
-  def anything = add("(.*)")
+  def multiple(value: String) = add(s"${quote(value)}+")
+  def many = multiple _
 
+  def anything = add("(.*)")
   def anythingBut(value: String) = add(s"([^${quote(value)}]*)")
 
   def something = add("(.+)")
-
   def somethingBut(value: String) = add(s"([^${quote(value)}]+)")
 
   def tab = add("\\t")
-
   def word = add("\\w+")
 
   def anyOf(value: String) = add(s"[${quote(value)}]")
-
   def any = anyOf _
 
   def lineBreak = add("(\\n|(\\r\\n))")
-
   def br = lineBreak
 
   def startOfLine(enable: Boolean = true) = copy(prefix = if (enable) "^" else "")
-
   def endOfLine(enable: Boolean = true) = copy(suffix = if (enable) "$" else "")
 
   def or(value: String) = copy("(" + prefix, expression + ")|(" + value, ")" + suffix)
-
   def or(value: VerbalExpression): VerbalExpression = or(value.expression)
 
   def range(args: Any*) = add(s"[${(args grouped 2 map {_ mkString "-"}).mkString}]")
@@ -56,31 +50,23 @@ case class VerbalExpression(prefix: String = "", expression: String = "", suffix
     'U' -> Pattern.UNICODE_CHARACTER_CLASS
   )
 
-  def modify(modifier: Char, add: Boolean) =  copy(modifiers = if (add) modifiers | charModToInt(modifier) else modifiers ^ charModToInt(modifier))
+  def addModifier(modifier: Char) = copy(modifiers = modifiers | charModToInt(modifier))
+  def removeModifier(modifier: Char) = copy(modifiers = modifiers ^ charModToInt(modifier))
 
-  def addModifier(modifier: Char) = modify(modifier, true)
-
-  def removeModifier(modifier: Char) = modify(modifier, false)
+  def modify(modifier: Char, add: Boolean) = if (add) addModifier(modifier) else removeModifier(modifier)
 
   def withAnyCase(enable: Boolean = true) = modify('i', enable)
-
   def searchOneLine(enable: Boolean = true) = modify('m', enable)
-
   def stopAtFirst(enable: Boolean = false) = modify('g', enable)
 
   def repeatPrevious(n: Int) = ???
-
   def repeatPrevious(atleast: Int, atmost: Int) = ???
 
-  def multiple(value: String) = add(s"${quote(value)}+")
-
   def beginCapture = add("(")
-
   def endCapture = add(")")
 
-  def pattern = Pattern.compile(prefix + expression + suffix, modifiers)
-
   def test(toTest: String) = Pattern.matches(toString, toTest)
+  def pattern = Pattern.compile(prefix + expression + suffix, modifiers)
 
   override def toString = pattern.pattern()
 }
